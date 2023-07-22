f1038f6aaeb592e4f0bdc3f789c32331db0eadbc
vladimir <wowanderful@gmail.com>
2023-07-22 05:06:26 +0300

    video finished

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,9 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import {playVideo} from './modules/playvideo';
+
+playVideo();
 
 // ---------------------------------
 
---
+++ source/js/modules/playvideo.js
@@ -0,0 +1,25 @@
+const videoContainer = document.querySelector('.gym-video__container');
+const button = document.querySelector('.button-play');
+const poster = document.querySelector('.video-preload');
+
+const onButtonClick = () => {
+  poster.classList.add('img-hidden');
+  const iframe = document.createElement('iframe');
+  iframe.classList.add('video');
+  iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
+  iframe.setAttribute('autoplay', '1');
+  iframe.setAttribute('width', '364');
+  iframe.setAttribute('height', '228');
+  videoContainer.appendChild(iframe);
+  button.classList.add('button-hidden');
+};
+
+const playVideo = () => {
+  button.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    onButtonClick();
+  }
+  );
+};
+
+export {playVideo};
