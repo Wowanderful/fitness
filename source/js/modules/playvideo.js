5c36dea59c0ac7be7f31c2fe19dd7a0cadf02cbd
vladimir <wowanderful@gmail.com>
2023-08-09 20:43:01 +0300

    bem blocks edited

--- source/js/modules/playvideo.js
+++ source/js/modules/playvideo.js
@@ -1,17 +1,17 @@
-const videoContainer = document.querySelector('.gym-video__container');
-const button = document.querySelector('.button-play');
-const poster = document.querySelector('.gym__video-preload');
+const videoContainer = document.querySelector('.photo-video__video-container');
+const button = document.querySelector('.gym__button-play');
+const poster = document.querySelector('.photo-video__video-preload');
 
 const onButtonClick = () => {
-  poster.classList.add('img-hidden');
+  poster.classList.add('photo-video__img-hidden');
   const iframe = document.createElement('iframe');
-  iframe.classList.add('video');
+  iframe.classList.add('photo-video__video');
   iframe.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?autoplay=1');
   iframe.setAttribute('autoplay', '1');
   iframe.setAttribute('width', '364');
   iframe.setAttribute('height', 'auto');
   videoContainer.appendChild(iframe);
-  button.classList.add('button-hidden');
+  button.classList.add('photo-video__button-hidden');
 };
 
 const playVideo = () => {
